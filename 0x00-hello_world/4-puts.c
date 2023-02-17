ed60a4e3a33cf8ad784e31b17810d5bbdc0ae98c
Mbulelo Kondile <mbulelo.mbucha@gmail.com>
2023-02-16 17:12:35 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - using puts function to print a statement
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
