60e61b56ac0848dffbeb051a13e42ce57b0ea14b
Mbulelo Kondile <mbulelo.mbucha@gmail.com>
2023-02-23 18:32:09 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*print_triangle - prints a triangle
+*@size:size parameter of triangle
+*Return: void
+*/
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
